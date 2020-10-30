a54660ac145055bb19f5e6da2dd590d5b9f674be
milesdavis71 <hevesiistvan71@hotmail.com>
2020-10-30 22:08:31 +0100

    Balkezesek kész.

--- balkezesek.py
+++ balkezesek.py
@@ -1,8 +1,19 @@
 import pandas as pd
 
-
 df = pd.read_csv("balkezesek.csv", sep=';', header=0)
+df['elsoev'] = pd.to_datetime(df['elso']).dt.year
+df['utolsoev'] = pd.to_datetime(df['utolso']).dt.year
+
 print(len(df))
 oktoberbenPalyaraLeptek = df[df['utolso'].str.contains("1999-10")]
-print((oktoberbenPalyaraLeptek['nev'].map(str) +", "+ ((oktoberbenPalyaraLeptek['magassag'].astype(float)*2.54).round(1)).map(str)+" cm").to_string(index=False))
+print((oktoberbenPalyaraLeptek['nev'].map(str) + ", " + (
+    (oktoberbenPalyaraLeptek['magassag'].astype(float) * 2.54).round(1)).map(str) + " cm").to_string(index=False))
 
+input_ev = input('Adj meg egy számot!')
+print('Köszi!')
+while not 1990 <= int(input_ev) <= 1999:
+    print('szar')
+    input('Adj másik számot...')
+
+input_ev_jatszott = df[(df['elsoev'].astype(int) >= int(input_ev)) & (df['utolsoev'].astype(int) <= int(input_ev))].mean()
+print(str(round(input_ev_jatszott['suly'], 2))+' font')
